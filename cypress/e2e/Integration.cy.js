e942efeb0baa1b2df7f05148e0893855e96f08b1
Lucas Gustavo <lucas.gustavo.santos@hotmail.com>
2022-08-24 20:11:37 -0300

    Cria teste de POST

--- cypress/e2e/Integration.cy.js
+++ cypress/e2e/Integration.cy.js
@@ -9,10 +9,39 @@ describe('Texo IT APIs', () => {
             json: true,
 
         }).then((resp) => {
-            const email = resp.body[0].email
-            expect(email).to.eq(email)
-            expect(resp.status).to.eq (200)
+            const emailRequest = resp.body[0].email
+            expect(emailRequest).to.eq(email)
+            expect(resp.status).to.eq(200)
         })
+    })
+
+    it('POST - Criar usuário', () => {
+        const nameURL = 'users'
+        cy.request({
+            method: 'POST',
+            url: Cypress.config().baseUrl + nameURL,
+            body: {
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "street": "Kulas Light",
+                "suite": "Apt. 556",
+                "city": "Gwenborough",
+                "zipcode": "92998-3874",
+                "lat": "-37.3159",
+                "lng": "81.1496",
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "name": "Romaguera-Crona",
+                "catchPhrase": "Multi-layered client-server neural-net",
+                "bs": "harness real-time e-markets"
+            }
+        }).then((resp) => {
+            const idRequest = resp.body.id
+            expect(idRequest).to.eq(11)
+            expect(resp.status).to.eq(201)
+        })
+
     })
 })
 
