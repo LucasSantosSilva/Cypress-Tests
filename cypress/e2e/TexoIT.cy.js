8377338a01e6a2777625ea714b3a04ff226d0e2a
Lucas Gustavo <lucas.gustavo.santos@hotmail.com>
2022-08-24 00:33:38 -0300

    Cria teste para verificar elemento 6 do array

--- cypress/e2e/TexoIT.cy.js
+++ cypress/e2e/TexoIT.cy.js
@@ -5,6 +5,7 @@ import AlbumsphtosPage from "../../PageObject/AlbumsphotosPage.js"
 const home = new HomePage();
 const guide = new GuidePage();
 const albumsPhtos = new AlbumsphtosPage();
+var arrayJson = [];
 
 describe('Texo IT', () => {
   it('Verifica se acessa o menu guide', () => {
@@ -23,8 +24,22 @@ describe('Texo IT', () => {
   it('ler HTML e salvar dados em json', () => {
     cy.get("pre").then(($el) => {
       const text = $el.text(); // Obtemos o texto
-      const separeTextToJson = JSON.parse(text) // transforma em JSON
-      expect(separeTextToJson).to.not.equal(null);
+      arrayJson = JSON.parse(text) // transforma em JSON
+      expect(arrayJson).to.not.equal(null);
     })
+  })
+
+  it('Verifica elemento 6 do array', () => {
+    for (let index = 0; index < arrayJson.length; index++) {
+      if (index == 5) {
+        const element = arrayJson[index];
+        const convertToString = JSON.stringify(element);
+        expect(convertToString)
+        .contains('accusamus ea aliquid et amet sequi nemo')
+        .contains('https://via.placeholder.com/600/56a8c2')
+        .contains('https://via.placeholder.com/150/56a8c2')
+        .contains('"albumId":1')
+      }
+    }
   })
 })
